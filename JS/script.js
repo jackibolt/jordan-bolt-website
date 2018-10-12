0d197d8d06a552b211a06271d6a14dc562d357d4
Jacki Bolt <bolt.jacqueline@gmail.com>
2018-10-12 12:18:18 -0400

    restyled

---
+++ JS/script.js
@@ -0,0 +1,20 @@
+
+console.log('it works');
+
+// variables
+const loader = document.querySelector('.loader');
+const header = document.querySelector('header');
+
+
+header.style.display = 'none';
+
+// close loader + show header
+const antiLoader = () => {
+  loader.style.display = 'none';
+  header.style.display = 'block';
+}
+    // function delay
+    setTimeout(antiLoader, 4500);
+
+
+window.addEventListener("hashchange", function() { scrollBy(0, -90) })
